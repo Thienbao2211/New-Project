406cc7c75c7a77067bffb5ae769338b157205392
Thienbao2211 <thomasnguyen22111b@gmail.com>
2025-07-03 15:57:54 +0700

    Main Page

--- script/main.js
+++ script/main.js
@@ -1,42 +1,83 @@
 // Get API
+const api_key = "0506601f30e5459212136df9e64d55b5";
+let apiKeyPopular = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}`;
+let apiKeyTopRated = `https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}`;
+let apiKeyImg = "https://image.tmdb.org/t/p/w300";
 
-// const TMDB_API_KEY_POPULAR_MOVIES = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
-// const TMDB_API_KEY_TRENDING_MOVIES = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
-
-// Get elements
-
-// let imgCardTrendingM = document.querySelector("#img-card-trending");
-// let imgCardPopularM = document.querySelector("#img-card-popular");
-
-// let labelCardTrending = document.querySelector("#label-card-trending");
-// let labelCardPopular = document.querySelector("#label-card-popular");
-
-let imgCardPopular = document.querySelectorAll("#img-card-popular");
-let labelCardPopular = document.querySelectorAll("#label-card-popular");
-
-let movieList = document.querySelector(".card-list");
+let movieListPopular = document.querySelector(".card-list-popular");
+let movieListTopRated = document.querySelector(".card-list-top-rated");
 
 // Display The API
 
-async function movie() {
-  const request =
-    await "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
-  const movies = await request.json();
-  console.log(movies);
-  makeMoviesCard(movies);
-}
+fetch(apiKeyTopRated)
+  .then((res) => res.json())
+  .then((data) => {
+
+    console.log(data.results);
+
+    // Top Rated Movie List
+    
+    for (let i in data.results) {
+      let cardMovieTopRated = document.createElement("div");
+
+      let labelMovieTopRated = document.createElement("p");
+      let imgMovieTopRated = document.createElement("img");
+
+      labelMovieTopRated.innerHTML = data.results[i].title;
+      imgMovieTopRated.src = apiKeyImg + data.results[i].poster_path + api_key;
+
+      movieListTopRated.appendChild(cardMovieTopRated);
+
+      cardMovieTopRated.appendChild(imgMovieTopRated);
+      cardMovieTopRated.appendChild(labelMovieTopRated);
+      
+      labelMovieTopRated.style.color = "white";
+      imgMovieTopRated.style.borderRadius = "15px";
+      imgMovieTopRated.addEventListener ("mouseover", () => {
+        imgMovieTopRated.style.zoom = "110%";
+      })
+      imgMovieTopRated.addEventListener ("mouseout", () => {
+        imgMovieTopRated.style.zoom = "0";
+      })
+      
+      movieListTopRated.style.display = "flex";
+      movieListTopRated.style.gap = "20px";
+      movieListTopRated.style.marginBottom = "20px";
+
+    }
+  });
+
+fetch(apiKeyPopular)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data.results);
+
+    for (let i in data.results) {
+      let cardMoviePopular = document.createElement("div");
+
+      let labelMoviePopular = document.createElement("p");
+      let imgMoviePopular = document.createElement("img");
+
+      labelMoviePopular.innerHTML = data.results[i].title;
+      imgMoviePopular.src = apiKeyImg + data.results[i].poster_path + api_key;
+
+      movieListPopular.appendChild(cardMoviePopular);
+
+      cardMoviePopular.appendChild(imgMoviePopular);
+      cardMoviePopular.appendChild(labelMoviePopular);
+
+      labelMoviePopular.style.color = "white";
+      imgMoviePopular.style.borderRadius = "15px";
+      imgMoviePopular.addEventListener("mouseover", () => {
+        imgMoviePopular.style.zoom = "110%";
+      });
+      imgMoviePopular.addEventListener("mouseout", () => {
+        imgMoviePopular.style.zoom = "0";
+      });
+
+      movieListPopular.style.display = "flex";
+      movieListPopular.style.gap = "20px";
+    }
+  });
 
 // Function Make Movie Card
-
-function makeMoviesCard(movies) {
-  for (let movie of movies) {
-    let imageMovieSrc = movie.show.image.medium;
-    let labelMovieText = movie.show.text.medium;
-
-    imgCardPopular.src = imageMovieSrc;
-    labelCardPopular.text = labelMovieText;
-
-    // movieList.appendChild(imageMovieCard);
-    // movieList.appendChild(labelMovieCard);
-  }
-}
