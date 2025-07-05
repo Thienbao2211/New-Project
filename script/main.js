f63c9e02c265ae1f0234ac49614d47e2762fa343
Thienbao2211 <thomasnguyen22111b@gmail.com>
2025-07-05 15:20:42 +0700

    Movie Overview Page

--- script/main.js
+++ script/main.js
@@ -2,60 +2,16 @@
 const api_key = "0506601f30e5459212136df9e64d55b5";
 let apiKeyPopular = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}`;
 let apiKeyTopRated = `https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}`;
-let apiKeyImg = "https://image.tmdb.org/t/p/w300";
+let apiKeyUpcoming = `https://api.themoviedb.org/3/movie/upcoming?api_key=${api_key}`;
+let apiKeyImg = "https://image.tmdb.org/t/p/w200";
 
 let movieListPopular = document.querySelector(".card-list-popular");
 let movieListTopRated = document.querySelector(".card-list-top-rated");
+let movieListTrending = document.querySelector(".card-list-trending");
 
 // Display The API
 
-fetch(apiKeyTopRated)
-  .then((res) => res.json())
-  .then((data) => {
-
-    console.log(data.results);
-
-    // Top Rated Movie List
-    
-    for (let i in data.results) {
-      let cardMovieTopRated = document.createElement("div");
-
-      let labelMovieTopRated = document.createElement("p");
-      let imgMovieTopRated = document.createElement("img");
-
-      labelMovieTopRated.innerHTML = data.results[i].title;
-      imgMovieTopRated.src = apiKeyImg + data.results[i].poster_path + api_key;
-
-      movieListTopRated.appendChild(cardMovieTopRated);
-
-      cardMovieTopRated.appendChild(imgMovieTopRated);
-      cardMovieTopRated.appendChild(labelMovieTopRated);
-      
-      labelMovieTopRated.style.color = "white";
-      imgMovieTopRated.style.borderRadius = "15px";
-      imgMovieTopRated.addEventListener ("mouseover", () => {
-        imgMovieTopRated.style.zoom = "110%";
-      })
-      imgMovieTopRated.addEventListener ("mouseout", () => {
-        imgMovieTopRated.style.zoom = "0";
-      })
-      
-      movieListTopRated.style.display = "flex";
-      movieListTopRated.style.gap = "20px";
-      movieListTopRated.style.marginBottom = "20px";
-
-      // Get API ID
-
-      cardMovieTopRated.addEventListener("click", () => {
-        let apiMovieId = data.results[i].id;
-        localStorage.setItem("Id", apiMovieId);
-        console.log(apiMovieId);
-
-        window.location.href = "../movie-overview.html"
-      })
-
-    }
-  });
+// Popular Movie List
 
 fetch(apiKeyPopular)
   .then((res) => res.json())
@@ -95,9 +51,102 @@ fetch(apiKeyPopular)
         localStorage.setItem("Id", apiMovieId);
         console.log(apiMovieId);
 
-        window.location.href = "../movie-overview.html"
+        window.location.href = "movie-overview.html"
       })
     }
   });
 
-// Function Make Movie Card
+  
+// Top Rated Movie List
+
+fetch(apiKeyTopRated)
+  .then((res) => res.json())
+  .then((data) => {
+
+    console.log(data.results);
+    
+    for (let i in data.results) {
+      let cardMovieTopRated = document.createElement("div");
+
+      let labelMovieTopRated = document.createElement("p");
+      let imgMovieTopRated = document.createElement("img");
+
+      labelMovieTopRated.innerHTML = data.results[i].title;
+      imgMovieTopRated.src = apiKeyImg + data.results[i].poster_path + api_key;
+
+      movieListTopRated.appendChild(cardMovieTopRated);
+
+      cardMovieTopRated.appendChild(imgMovieTopRated);
+      cardMovieTopRated.appendChild(labelMovieTopRated);
+      
+      labelMovieTopRated.style.color = "white";
+      imgMovieTopRated.style.borderRadius = "15px";
+      imgMovieTopRated.addEventListener ("mouseover", () => {
+        imgMovieTopRated.style.zoom = "110%";
+      })
+      imgMovieTopRated.addEventListener ("mouseout", () => {
+        imgMovieTopRated.style.zoom = "0";
+      })
+      
+      movieListTopRated.style.display = "flex";
+      movieListTopRated.style.gap = "20px";
+      movieListTopRated.style.marginBottom = "20px";
+
+      // Get API ID
+
+      cardMovieTopRated.addEventListener("click", () => {
+        let apiMovieId = data.results[i].id;
+        localStorage.setItem("Id", apiMovieId);
+        console.log(apiMovieId);
+
+        window.location.href = "movie-overview.html"
+      })
+
+    }
+  });
+
+
+// Upcoming Movie List
+
+fetch(apiKeyUpcoming)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data.results);
+
+    for (let i in data.results) {
+      let cardMovieUpcoming = document.createElement("div");
+
+      let labelMovieTrending = document.createElement("p");
+      let imgMovieTrending = document.createElement("img");
+
+      labelMovieTrending.innerHTML = data.results[i].title;
+      imgMovieTrending.src = apiKeyImg + data.results[i].poster_path + api_key;
+
+      movieListTrending.appendChild(cardMovieUpcoming);
+
+      cardMovieUpcoming.appendChild(imgMovieTrending);
+      cardMovieUpcoming.appendChild(labelMovieTrending);
+
+      labelMovieTrending.style.color = "white";
+      imgMovieTrending.style.borderRadius = "15px";
+      imgMovieTrending.addEventListener("mouseover", () => {
+        imgMoviePopular.style.zoom = "110%";
+      });
+      imgMovieTrending.addEventListener("mouseout", () => {
+        imgMoviePopular.style.zoom = "0";
+      });
+
+      movieListTrending.style.display = "flex";
+      movieListTrending.style.gap = "20px";
+
+      // Get API ID
+
+      cardMovieUpcoming.addEventListener("click", () => {
+        let apiMovieId = data.results[i].id;
+        localStorage.setItem("Id", apiMovieId);
+        console.log(apiMovieId);
+
+        window.location.href = "movie-overview.html"
+      })
+    }
+  });
--- script/movie-overview.js
+++ script/movie-overview.js
@@ -4,6 +4,8 @@ const api_key = "0506601f30e5459212136df9e64d55b5";
 // let apiKeyId = `https://api.themoviedb.org/3/movie/{movie_id}/external_ids?api_key=${api_key}`;
 let apiKeyMovieListTopRated = `https://api.themoviedb.org/3/movie/top_rated?api_key=${api_key}`;
 let apiKeyMovieListPopular = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}`;
+let apiKeyMovieListUpcoming = `https://api.themoviedb.org/3/movie/upcoming?api_key=${api_key}`;
+
 let apiKeyImgBackdropPath = `http://image.tmdb.org/t/p/w1920/`;
 let apiKeyImgPosterPath = `http://image.tmdb.org/t/p/w300/`;
 
@@ -14,9 +16,61 @@ let posterImg = document.querySelector(".poster-movie-overview");
 let titleMovie = document.querySelector(".title-movie-overview");
 let labelMovie = document.querySelector(".label-movie-overview");
 
-// Fetch API
+// Fetch API Top Rated Movie
 
 fetch(apiKeyMovieListTopRated)
+    .then((res) => res.json())
+    .then((data) => {
+
+        console.log(data.results);
+        let apiKeyId = localStorage.getItem("Id");
+        console.log(apiKeyId);
+
+        let findIdMovie = data.results.find(key => key.id == apiKeyId);
+        let backdropPathMovie = apiKeyImgBackdropPath + findIdMovie.backdrop_path + api_key;
+        let posterPathMovie = apiKeyImgPosterPath + findIdMovie.poster_path + api_key;
+        let titlePathMovie = findIdMovie.title;
+        let subtitleMovie = findIdMovie.overview;
+
+        backgroundImg.src = backdropPathMovie;
+        posterImg.src = posterPathMovie;
+        titleMovie.innerHTML = titlePathMovie;
+        labelMovie.innerHTML = subtitleMovie;
+
+        console.log(backdropPathMovie);
+
+    })
+
+
+// Fetch API Popular Movie
+
+fetch(apiKeyMovieListPopular)
+    .then((res) => res.json())
+    .then((data) => {
+
+        console.log(data.results);
+        let apiKeyId = localStorage.getItem("Id");
+        console.log(apiKeyId);
+
+        let findIdMovie = data.results.find(key => key.id == apiKeyId);
+        let backdropPathMovie = apiKeyImgBackdropPath + findIdMovie.backdrop_path + api_key;
+        let posterPathMovie = apiKeyImgPosterPath + findIdMovie.poster_path + api_key;
+        let titlePathMovie = findIdMovie.title;
+        let subtitleMovie = findIdMovie.overview;
+
+        backgroundImg.src = backdropPathMovie;
+        posterImg.src = posterPathMovie;
+        titleMovie.innerHTML = titlePathMovie;
+        labelMovie.innerHTML = subtitleMovie;
+
+        console.log(backdropPathMovie);
+
+    })
+
+
+// Fetch API Upcoming Movie
+
+fetch(apiKeyMovieListPopular)
     .then((res) => res.json())
     .then((data) => {
 
